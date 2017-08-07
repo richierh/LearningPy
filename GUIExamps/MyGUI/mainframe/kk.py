9b755302ec8c37be9882dafe217c9e75b9e7259a
richie <pmc.computer@yahoo.com>
2017-08-07 14:33:13 +0700

    my commit

--- GUIExamps/MyGUI/mainframe/kk.py
+++ GUIExamps/MyGUI/mainframe/kk.py
@@ -3,4 +3,5 @@ import wx
 class size(wx.Frame):
     def __init__(self,*args,**kwds):
         super(size,self).__init__(*args,**kwds)
+        self.SetSizeHints( wx.Size( 500,300 ), wx.DefaultSize )
         print ("hello")
