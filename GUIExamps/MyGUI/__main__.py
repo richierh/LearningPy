8664b53fb0737589d66b55d199ad7fae7c04698b
richie <pmc.computer@yahoo.com>
2017-08-07 22:39:15 +0700

    dfd

--- GUIExamps/MyGUI/__main__.py
+++ GUIExamps/MyGUI/__main__.py
@@ -1,12 +1,12 @@
 #! /usr/bin/python
 import wx
 import gettext
-from mainframe.form import * 
+import mainframe.form
 
 
 class MyApp(wx.App):
     def OnInit(self):
-        self.frame = MyFrame1(None)
+        self.frame = mainframe.form.MyFrame1(None)
         self.SetTopWindow(self.frame)
         self.frame.Show()
         return True
@@ -14,7 +14,5 @@ class MyApp(wx.App):
 
 if __name__ == "__main__":
     gettext.install("app") # replace with the appropriate catalog name
-    
     app = MyApp(0)
     app.MainLoop()
-
