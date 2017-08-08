420a5598ef19b83503cc44ea825b289d24ef04ab
richie <pmc.computer@yahoo.com>
2017-08-08 10:01:26 +0700

    ccommi

--- GUIExamps/MyGUI/__main__.py
+++ GUIExamps/MyGUI/__main__.py
@@ -1,13 +1,13 @@
 #! /usr/bin/python
 import wx
 import gettext
-import mainframe.form
-import controller
-
+from mainframe.form import* 
+import mainframe
+ 
 
 class MyApp(wx.App):
     def OnInit(self):
-        self.frame = mainframe.form.MyFrame1(None)
+        self.frame = MyFrame1(None)
         self.SetTopWindow(self.frame)
         self.frame.Show()
         return True
@@ -15,5 +15,6 @@ class MyApp(wx.App):
 
 if __name__ == "__main__":
     gettext.install("app") # replace with the appropriate catalog name
+
     app = MyApp(0)
     app.MainLoop()
---
+++ GUIExamps/MyGUI/db/connection.py
@@ -0,0 +1,10 @@
+from pony.orm import *
+from ormpony import *
+
+#db = Database()
+db.bind(provider='sqlite', filename='mydatabase.sqlite', create_db=True)
+show(DataPersonal)
+sql_debug(True)
+#db.generate_mapping(False)
+#db.generate_mapping(create_tables=True)
+
